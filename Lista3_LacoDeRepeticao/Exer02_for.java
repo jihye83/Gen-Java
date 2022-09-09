f5e65504043d8105aef4bc52d8b8a9e1d45863f2
ji hye <jihye83@hotmail.com>
2022-09-09 13:30:38 -0300

    Exer02 feito

--- Lista3_LacoDeRepeticao/Exer02_for.java
+++ Lista3_LacoDeRepeticao/Exer02_for.java
@@ -1,13 +1,29 @@
 package Lista3_LacoDeRepeticao;
 
+import java.util.Scanner;
+
 public class Exer02_for {
 	/*
 	 * 2- Ler 10 números e imprimir quantos são pares e quantos são ímpares.
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		int i, num, contPar = 0, contImpar = 0;
+
+		for (i = 0; i < 10; i++) {
+			System.out.println("\nDigite o Numero: ");
+			num = sc.nextInt();
+			if (num % 2 == 0) {
+				contPar++;
+			} else {
+				contImpar++;
+			}
+		}
+
+		System.out.println("\nContagem dos numeros Pares: " + contPar);
+		System.out.println("\nContagem dos numeros Impares: " + contImpar);
 	}
 
 }
