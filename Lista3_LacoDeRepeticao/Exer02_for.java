013db839ddf5c4477f47c5a90cfbc270bb88fd0b
ji hye <jihye83@hotmail.com>
2022-09-09 13:22:17 -0300

    ajuste no exer03_while

---
+++ Lista3_LacoDeRepeticao/Exer02_for.java
@@ -0,0 +1,13 @@
+package Lista3_LacoDeRepeticao;
+
+public class Exer02_for {
+	/*
+	 * 2- Ler 10 números e imprimir quantos são pares e quantos são ímpares.
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Lista3_LacoDeRepeticao/Exer03_While.java
+++ Lista3_LacoDeRepeticao/Exer03_While.java
@@ -16,20 +16,27 @@ public class Exer03_While {
 
 		System.out.println("\nDigite a sua idade; ");
 		idade = sc.nextInt();
+		// coloquei o while dentro do if. Porque nao queria q por qualquer motivo
+		// ele imprimisse o a saida com total zerado. Entao se o usuario digita
+		// na primeira -99 ele imprimi "saiu do loop".
+		if (idade != -99) {
+			while (idade != -99) {
+				// nao queria que contasse a idade zero.
+				if (idade > 0 && idade < 21) {
+					contMenor++;
+				}
+				if (idade > 50) {
+					contMaior++;
+				}
+				System.out.println("\nDigite a sua idade; ");
+				idade = sc.nextInt();
+			}
 
-		while (idade != 0) {
-			if (idade < 21) {
-				contMenor++;
-			}
-			if (idade > 50) {
-				contMaior++;
-			}
-			System.out.println("\nDigite a sua idade; ");
-			idade = sc.nextInt();
+			System.out.println("\nTotal de pessoas com idade menores de 21 anos foi de " + contMenor);
+			System.out.println("\nTotal de psssoas com idade maiores de 50 anos foi de " + contMaior);
+		} else {
+			System.out.println("\nSaiu do Loop!");
 		}
-
-		System.out.println("\nTotal de pessoas com idade menores de 21 anos foi de " + contMenor);
-		System.out.println("\nTotal de psssoas com idade maiores de 50 anos foi de " + contMaior);
 	}
 
 }
