0e120a01dee338b5ea36943fcfb6e51b0f7ecffd
ji hye <jihye83@hotmail.com>
2022-09-13 19:18:11 -0300

    Exer Extra Completo

--- Lista4_ArrayVetorMatriz/Extra.java
+++ Lista4_ArrayVetorMatriz/Extra.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class Extra {
 	/*
 	 *5. Crie programas em Java que crie e exiba as seguintes figuras abaixo utilizando matrizes:
-	 *  *  *  *  *
-	 *  *        *
-	 *  *        *
-	 *  *  *  *  *
+	 * 1)* * * *   2)* * * *   3)*
+	 *   * * * *     *     *     * *
+	 *   * * * *     *     *     * * *
+	 *   * * * *     * * * *     * * * *
 	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		char [][] matriz = new char[4][4];
-		//char [][] matriz = {{'*','*','*','*'},{'*','*','*','*'},{'*','*','*','*'},{'*','*','*','*'}};
+	
+		char [][] matriz = {{'*','*','*','*'},{'*','*','*','*'},{'*','*','*','*'},{'*','*','*','*'}};
 		int i, j;
+		
+		/*
+	 	char [][] matriz = new char[4][4];
 		for (i = 0; i < 4; i++) {
 			for (j = 0; j < 4; j++) {
 				System.out.println("\nDigite o * : ");
@@ -23,7 +25,15 @@ public class Extra {
 				matriz[i][j] = c.charAt(0);
 			}
 		}
-
+		 */
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < 4; j++) {
+				System.out.print(matriz[i][j] + " ");		
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
 		for (i = 0; i < 4; i++) {
 			for (j = 0; j < 4; j++) {
 				if (i == 0 || i == 3 || j == 0 || j == 3) {
@@ -31,6 +41,18 @@ public class Extra {
 				} else {
 					System.out.print("  ");
 				}			
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		for (i = 0; i < 4; i++) {
+			for (j = 0; j < 4; j++) {
+				if (i > j || i == j) {
+					System.out.print(matriz[i][j] + " ");
+				}else {
+					System.out.print("  ");
+				} 		
 			}
 			System.out.println();
 		}
